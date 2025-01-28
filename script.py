e9aa303aacfe75f8ee76c34a949594e3b0fa1ecb
bakawap-dev <crazybakawap@gmail.com>
2025-01-28 23:10:21 +0700

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ def run():
         browser = p.chromium.launch(headless=True)  # Chạy không có giao diện (bắt buộc)
         page = browser.new_page()
         
-        page.goto("https://yyeumoney.com/example")  # Thay link bạn muốn tự động hóa
+        page.goto("https://funlink.io/PF8n8vb")  # Thay link bạn muốn tự động hóa
         page.wait_for_timeout(5000)  # Chờ 5s
         print("Title:", page.title())  # In tiêu đề trang
         
